324e7723fe1d7a851da0080c36656c03de22f4e0
RabronixCode <letscodeit10@gmail.com>
2024-12-21 17:21:24 +0100

    All the data and time needed

--- DesmieGreece/greece.py
+++ DesmieGreece/greece.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import os
 import shutil
 import time
+from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -55,11 +57,64 @@ for i, button in enumerate(downloads):
 
 
 # Rename downloaded files
-new_file_names = [f"file_{i+1}.xls" for i in range(len(downloads))]
+new_file_names = [f"file_{i+1}.xlsx" for i in range(len(downloads))]
 for i, filename in enumerate(os.listdir(download_dir)):
     old_path = os.path.join(download_dir, filename)
     new_path = os.path.join(download_dir, new_file_names[i])
     if os.path.isfile(old_path):
         shutil.move(old_path, new_path)
         print(f"Renamed: {old_path} to {new_path}")
+
+
+# Needed data
+buy = []
+sell = []
+buy_sell_max = []
+market_clearing_price = []
+time_column = []
+
+# Get a list of all Excel files in the folder
+excel_files = [file for file in os.listdir(download_dir) if file.endswith(('.xlsx', '.xlsm'))]
+
+# Process files in chunks of 7
+batch_size = 7
+for batch_start in range(0, len(excel_files), batch_size):
+    batch_files = excel_files[batch_start:batch_start + batch_size]
+    print(f"Processing batch: {batch_files}")
+    
+    for file_name in batch_files:
+        file_path = os.path.join(download_dir, file_name)
+        print(f"Processing file: {file_name}")
+        
+        # Open the Excel file
+        workbook = load_workbook(file_path)
+        time_data = []
+        i = 0
+        for sheet_name in workbook.sheetnames[:2]:
+            sheet = workbook[sheet_name]
+            
+            time_data = [str(sheet.cell(row=2, column=col).value) for col in range(1, 26)]
+            
+            if i == 0:
+                # Extract data from row 4, columns B through Y
+                buy.extend([sheet.cell(row=4, column=col).value for col in range(2, 26)])  # Columns B (2) to Y (25)
+            else:
+                sell.extend([sheet.cell(row=4, column=col).value for col in range(2, 26)])
+                market_clearing_price.extend([sheet.cell(row=7, column=col).value for col in range(2, 26)])
+            
+            i += 1
+        
+        parsed_date = datetime.strptime(time_data[0], "%Y-%m-%d %H:%M:%S")  # Parse the date string
+        day_month = parsed_date.strftime("%d %b")  # Format as '16 Dec'
+
+        for i in range(1, len(time_data)):
+            if int(time_data[i]) < 10:
+                if time_data[i] == '1':
+                    time_column.append(f'{day_month} 00 - 0{time_data[i]}')
+                else:
+                    time_column.append(f'{day_month} 0{time_data[i-1]} - 0{time_data[i]}')
+            elif int(time_data[i]) == 10:
+                time_column.append(f'{day_month} 0{time_data[i-1]} - {time_data[i]}')
+            else:
+                time_column.append(f'{day_month} {time_data[i-1]} - {time_data[i]}')
 
