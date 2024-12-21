6d3f35bf5caaaf1704c0c459c9249bcc9fe01e2c
RabronixCode <letscodeit10@gmail.com>
2024-12-21 15:51:29 +0100

    Download - Delete existing - Rename Files

--- DesmieGreece/greece.py
+++ DesmieGreece/greece.py
@@ -38,3 +38,28 @@ downloads = driver.find_elements(By.XPATH, f'//*[@class="portlet-body"]//a[conta
 #for d in downloads:
 #    print(d.text)
 
+# First always delete all files from the download_dir folder
+for filename in os.listdir(download_dir):
+    file_path = os.path.join(download_dir, filename)
+    try:
+        if os.path.isfile(file_path):  # Check if it's a file
+            os.remove(file_path)  # Delete the file
+            print(f"Deleted: {file_path}")
+    except Exception as e:
+        print(f"Failed to delete {file_path}: {e}")
+
+# Click on the link - then wait a bit
+for i, button in enumerate(downloads):
+    button.click()
+    time.sleep(5)
+
+
+# Rename downloaded files
+new_file_names = [f"file_{i+1}.xls" for i in range(len(downloads))]
+for i, filename in enumerate(os.listdir(download_dir)):
+    old_path = os.path.join(download_dir, filename)
+    new_path = os.path.join(download_dir, new_file_names[i])
+    if os.path.isfile(old_path):
+        shutil.move(old_path, new_path)
+        print(f"Renamed: {old_path} to {new_path}")
+
